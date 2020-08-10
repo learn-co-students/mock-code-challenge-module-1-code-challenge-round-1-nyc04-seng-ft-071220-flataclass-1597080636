7a4b4e88c95036feb8d5f160478907c15b819037
attenbl19 <attenboroughl@protonmail.com>
2020-08-10 14:37:09 -0400

    initialized Course class

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 class Tutor
-  
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+        @@all
+  end
+  binding.pry
 end
