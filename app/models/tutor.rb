a94acb931d7e51bdd76b144478fe6876fd9cb377
joshuadelapenha <joshuadelapenha@yahoo.com>
2020-08-10 14:02:10 -0400

    built deliverable basic methods

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,3 +1,18 @@
 class Tutor
-  
+
+    @@all = []
+
+    attr_accessor :name
+
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+
+    def self.all
+    @@all
+    end
+
 end
+
+
