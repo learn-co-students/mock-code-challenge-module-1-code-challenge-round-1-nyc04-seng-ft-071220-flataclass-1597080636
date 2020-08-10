176f73eba82db736d3bb3ec1d9a0db6b8154aeb3
BellaEvalle <bellaevalle@yahoo.com>
2020-08-10 15:48:51 -0400

    Done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,42 @@
 class Student
+# - `Student#initialize(name)`
+# - `Student` is initialized with a name (string)
+# - name **can be** changed after the `Student` is initialized
+# - `Student#name`
+# - returns the `Student`'s name
+# - `Student.all`
+# - returns an array of all the `Student` instances that have been initialized
+
+attr_accessor :student_name
+
+@@all = []
+
+    def initialize(student_name)
+        @student_name= student_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def course
+        Course.all
+        # - `Student#courses`
+        # - returns an array of all the `Course` instances for the `Student`.
+    end
+
+    def tutor
+        # - `Student#tutors`
+        Tutor.all
+        # - returns an array of all of the `Tutor` instances that are tutoring the `Student`.
+    end
+
+    def enrolled_fulltime
+        # - `Student#enrolled_fulltime?`
+        # - returns `true` if the `Student` has enrolled for at least 3 Courses, returns `false` otherwise
+    end
+
+
 
 end
--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,3 +1,40 @@
 class Tutor
   
+# - `Tutor#initialize(name)`
+# - `Tutor` is initialized with a name (string)
+# - name **can be** changed after the Tutor is initialized
+# - `Tutor#name`
+# - returns the Tutor's name
+# - `Tutor.all`
+# - returns an array of all the `Tutor` instances that have been initialized
+
+attr_accessor :tutor_name
+
+@@all = []
+
+    def initialize (tutor_name)
+        @tutor_name = tutor_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def student
+        Student.all
+        # - `Tutor#students`
+        # - returns an array of `Student` instances tutored by the `Tutor` instance.
+    end
+
+    def tutored_student
+        # - `Tutor#tutored_student?(student)`
+        # - a `Student` instance is the only argument
+        # - returns `true` if the `Tutor` has tutored this 
+        # `Student` (if there is a `Course` instance that has this `Tutor` and `Student`), 
+        # returns `false` otherwise
+    end
+
+
+
 end
