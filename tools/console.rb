b1e060ee965935099e2a347e9c42bccd0b62d6fa
BellaEvalle <bellaevalle@yahoo.com>
2020-08-10 15:48:23 -0400

    Done

--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,14 @@ def reload
   load 'config/environment.rb'
 end
 
+student1 = Student.new("Bella")
+student2 = Student.new("Brian")
+tutor1 = Tutor.new("Sylwia")
+tutor2 = Tutor.new("Eric")
+course1 = Course.new(student1, tutor1, "Science")
+
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
