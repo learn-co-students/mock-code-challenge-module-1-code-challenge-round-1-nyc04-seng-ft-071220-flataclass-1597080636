2f70a9d878abb786e187b309e4d4420ebfc648ba
wilsonng21 <wilsongch92@gmail.com>
2020-08-10 13:59:12 -0400

    initializers, attrs completed

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,18 @@
 class Course
 
+    attr_accessor :tutor, :student, :course_name 
+
+    @@all = []
+
+    def initialize(tutor_param, student_param, course_name_param)
+        @tutor = tutor_param
+        @student = student_param
+        @course_name = course_name_param
+    end
+
+    def self.all 
+        @@all 
+    end
+    
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,16 @@
 class Student
 
+    attr_accessor :name
+
+    @@all = []
+
+    def intialize(name_param)
+        @name = name_param 
+        @@all << self
+    end
+
+    def self.all 
+        @@all 
+    end
+
 end
